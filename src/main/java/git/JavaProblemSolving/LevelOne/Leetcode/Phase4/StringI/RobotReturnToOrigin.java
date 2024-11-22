e48808b294ae3494f8b4827a5603196c9f24d0e5
Mostafa Osman <mostafa.osman.fathi@gmail.com>
2024-11-22 17:06:23 +0200

    adding problems to phase 4 section 3

---
+++ src/main/java/git/JavaProblemSolving/LevelOne/Leetcode/Phase4/StringI/RobotReturnToOrigin.java
@@ -0,0 +1,18 @@
+package git.JavaProblemSolving.LevelOne.Leetcode.Phase4.StringI;
+
+public class RobotReturnToOrigin {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            if (moves.charAt(i) == 'R') {
+                ++x;
+            } else if (moves.charAt(i) == 'L') {
+                --x;
+            } else if (moves.charAt(i) == 'U') {
+                ++y;
+            } else --y;
+        }
+
+        return x == 0 && y == 0;
+    }
+}
